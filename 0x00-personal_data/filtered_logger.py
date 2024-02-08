8e6f7ca182e59b834b9320c4ac32bfe9d5f7aec1
Otonda1 <charlesotonda@gmai.com>
2024-02-08 08:59:56 +0000

    a function called filter_datum that returns the log message obfuscated:

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,9 +1,29 @@
 #!/usr/bin/env python3
 """
-filtered logger module
+The filtered logger module
 """
-
+from os import environ
+from mysql.connector import connection
 import re
+from typing import List
+import logging
+
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
+
+
+def get_db() -> connection.MySQLConnection:
+    """Return a connector to the database"""
+    username = environ.get("PERSONAL_DATA_DB_USERNAME", "root")
+    password = environ.get("PERSONAL_DATA_DB_PASSWORD", "")
+    db_host = environ.get("PERSONAL_DATA_DB_HOST", "localhost")
+    db_name = environ.get("PERSONAL_DATA_DB_NAME")
+    connector = connection.MySQLConnection(
+        user=username,
+        password=password,
+        host=db_host,
+        database=db_name
+    )
+    return connector
 
 
 def filter_datum(fields: List[str],
@@ -15,3 +35,53 @@ def filter_datum(fields: List[str],
         message = re.sub(f'{field}=(.*?){separator}',
                          f'{field}={redaction}{separator}', message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """Filters values in incoming log records"""
+        return filter_datum(self.fields, self.REDACTION,
+                            super().format(record), self.SEPARATOR)
+
+
+def get_logger() -> logging.Logger:
+    """Logs while obfuscating PII fields"""
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+    handler = logging.StreamHandler()
+    logger.addHandler(handler)
+    handler.setFormatter(RedactingFormatter(PII_FIELDS))
+
+    return logger
+
+
+def main() -> None:
+    """The main function"""
+    db = get_db()
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM users;")
+    headers = [field[0] for field in cursor.description]
+    logger = get_logger()
+    for row in cursor:
+        info_answer = ""
+        for f, p in zip(row, headers):
+            info_answer += f"{p}={(f)};"
+        logger.info(info_answer)
+    cursor.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    main()
