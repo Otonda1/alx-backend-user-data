31bc258b993f8852d922c275f4371405a1992ed9
root <root@DESKTOP-ICK7MPV>
2024-02-13 21:35:10 +0300

    requirement check

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,8 +1,9 @@
-#!/usr/bi/env python3
+#!/usr/bin/env python3
 """ this module conatins AUTH class
 """
 from flask import request
 from typing import List, TypeVar
+
 
 class Auth:
     """ Auth class
