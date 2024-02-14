027524984454a4bd0df7816f7ec2b81dc5c02770
root <root@DESKTOP-ICK7MPV>
2024-02-15 00:16:21 +0300

    Introduces a new class named BasicAuth

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -14,9 +14,12 @@ app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 
-auth = getenv("AUTH_TYPE")
+auth_type = getenv("AUTH_TYPE")
 
-if auth:
+if auth_type and auth_type.lower() == "basic_auth":
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
+else:
     from api.v1.auth.auth import Auth
     auth = Auth()
 
---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+class inherits from Auth
+"""
+from api.v1.auth.auth import Auth
+
+class BasicAuth(Auth):
+    """
+    class inherits from Auth
+    """
+    pass
