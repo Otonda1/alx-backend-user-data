9312c969229ec443185475b7cc32739fa10d4e4f
root <root@DESKTOP-ICK7MPV>
2024-02-18 00:38:19 +0300

    check adjustment

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,24 +1,14 @@
 #!/usr/bin/env python3
+"""Defines SessionAuth class that represents
+   a custom session authenticator
 """
-introduction of new class
 
-"""
 from api.v1.auth.auth import Auth
 
 
 class SessionAuth(Auth):
-    """SessionAuth class
-
-    This class represents the session-based
-    authentication mechanism.
-    It is responsible for managing user
-    sessions and verifying session tokens.
-
+    """
+    This class represents the session-based authentication mechanism.
+    It is responsible for managing user sessions and verifying session tokens.
     """
     pass
-
-
-if issubclass(SessionAuth, Auth):
-    print("SessionAuth is a subclass of Auth")
-else:
-    print("SessionAuth is not a subclass of Auth")
