778057a8d194b932515b8b815a8e4bbce1deb2fb
root <root@DESKTOP-ICK7MPV>
2024-02-18 00:32:37 +0300

    check adjustment

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,8 +1,21 @@
 #!/usr/bin/env python3
 """
 introduction of new class
+
 """
 from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """SessionAuth class
+
+    This class represents the session-based
+    authentication mechanism.
+    It is responsible for managing user
+    sessions and verifying session tokens.
+
+    """
+    pass
 
 
 class SessionAuth(Auth):
