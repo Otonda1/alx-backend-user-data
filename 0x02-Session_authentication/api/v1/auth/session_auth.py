1583bfa316a7e2f40c388eb2f68066f355f73743
root <root@DESKTOP-ICK7MPV>
2024-02-18 15:08:42 +0300

    updating the SessionAuth class

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -4,6 +4,7 @@
 """
 
 from api.v1.auth.auth import Auth
+from uuid import uuid4
 
 
 class SessionAuth(Auth):
@@ -11,4 +12,15 @@ class SessionAuth(Auth):
     This class represents the session-based authentication mechanism.
     It is responsible for managing user sessions and verifying session tokens.
     """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """
+        Creates a new session for a user
+        """
+        if user_id is None or not isinstance(user_id, str):
+            return None
+
+        session_id = str(uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
