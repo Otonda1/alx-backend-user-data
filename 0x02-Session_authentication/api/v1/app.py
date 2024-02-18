01beecc0ab127ab2c6b509aec56a9b49be4046e8
root <root@DESKTOP-ICK7MPV>
2024-02-18 17:04:33 +0300

    updates the before request method

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -57,10 +57,13 @@ def before_request() -> None:
     else:
         if auth.require_auth(request.path, ['/api/v1/status/',
                                             '/api/v1/unauthorized/',
-                                            '/api/v1/forbidden/']):
+                                            '/api/v1/forbidden/',
+                                            '/api/v1/auth_session/login/']):
             if auth.authorization_header(request) is None:
                 abort(401)
-            if auth.current_user(request) is None:
+            elif auth.session_cookie(request) is None:
+                abort(403)
+            elif auth.current_user(request) is None:
                 abort(403)
         request.current_user = auth.current_user(request)
 
