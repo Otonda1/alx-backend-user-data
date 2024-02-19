1483f4fc794aabe0ed3446dccf9298f9a5c2fd68
root <root@DESKTOP-ICK7MPV>
2024-02-19 23:21:46 +0300

    introduces new view for session authentication

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -1,31 +1,44 @@
 #!/usr/bin/env python3
-"""
-this module defines the login route
-"""
 
+"""
+Module to handle all routes for session auth
+"""
 from api.v1.views import app_views
-from flask import jsonify, request, abort
+from flask import request, jsonify, abort
 from models.user import User
 import os
 
+
 @app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
 def login():
-    """ defins a path for the login route """
-    mail = request.form.get('email')
-    pwd = request.form.get('password')
-    if mail is None or mail == "":
+    """The login method"""
+    email = request.form.get('email', None)
+    password = request.form.get('password', None)
+    if not email:
         return jsonify({"error": "email missing"}), 400
-    if pwd is None or pwd == "":
+    if not password:
         return jsonify({"error": "password missing"}), 400
-    users = User.search({'email': mail})
-    if len(users) == 0:
+    users = User.search({"email": email})
+    if not users:
         return jsonify({"error": "no user found for this email"}), 404
     for user in users:
-        if not user.is_valid_password(pwd):
+        if not user.is_valid_password(password):
             return jsonify({"error": "wrong password"}), 401
         from api.v1.app import auth
         session_id = auth.create_session(user.id)
+        session_name = os.getenv("SESSION_NAME")
         resp = jsonify(user.to_json())
-        session_name = os.getenv('SESSION_NAME')
         resp.set_cookie(session_name, session_id)
         return resp
+
+
+@app_views.route('/auth_session/logout',
+                 methods=['DELETE'],
+                 strict_slashes=False)
+def logout():
+    """The logout method"""
+    from api.v1.app import auth
+    logout = auth.destroy_session(request)
+    if logout:
+        return jsonify({}), 200
+    abort(404)
