20079a46574a1f6f200e713f28c951a32c30fdfa
root <root@DESKTOP-ICK7MPV>
2024-02-21 11:21:24 +0300

    validates if the user has valid login

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -35,3 +35,15 @@ class Auth:
         except NoResultFound:
             _hash_password(password)
             return self._db.add_user(email, _hash_password(password))
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """Validate login
+        """
+        dB = DB()
+        try:
+            usr = dB.find_user_by(email=email)
+            if usr:
+                bcrypt.checkpw(password.encode('utf-8'), usr.hashed_password)
+                return True
+        except Exception as e:
+            return False
